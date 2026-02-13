c1a0de27326e053391d84b1f1424675b75b85ce4
Vong sokpheak <vongsokpheak4@gmail.com>
2026-02-13 15:10:30 +0700

    updated!

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  base: '/ReportApp/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    // នេះគឺជាកន្លែងវេទមន្ត! 🌟
+    // Netlify តែងតែមាន variable ឈ្មោះ NETLIFY=true ពេលកំពុង Build
+    base: process.env.NETLIFY ? '/' : '/ReportApp/',
+    
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+  }
 })
