eea7d1bf9365e9cce3821041add7ac599b6be04a
povdaroee-collab <povdaroee@gmail.com>
2026-02-22 18:20:32 +0700

    Setup GitHub Actions for auto deployment

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => {
     ],
     // នេះគឺជាកន្លែងវេទមន្ត! 🌟
     // Netlify តែងតែមាន variable ឈ្មោះ NETLIFY=true ពេលកំពុង Build
+    // ដូច្នេះបើ Deploy លើ Netlify វាប្រើ '/' រីឯ GitHub Pages ប្រើ '/ReportApp/'
     base: process.env.NETLIFY ? '/' : '/ReportApp/',
     
     resolve: {
