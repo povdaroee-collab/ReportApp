dbca276023c96d552c5790eacf8b277b38caade2
Rieakear-app <daropovkl85@gmail.com>
2026-03-22 09:34:37 +0700

    update new

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.26m9hbq83b8"
+    "revision": "0.kpuvqir26ck"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
